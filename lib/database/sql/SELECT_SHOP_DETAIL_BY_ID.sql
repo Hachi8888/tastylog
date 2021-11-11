c17aff230db73339e6d6c8768b2fc18a9fae0009
Hachi8888 <h.nana.420@gmail.com>
2021-11-11 23:25:15 +0900

    fixed sql

--- lib/database/sql/SELECT_SHOP_DETAIL_BY_ID.sql
+++ lib/database/sql/SELECT_SHOP_DETAIL_BY_ID.sql
@@ -6,7 +6,7 @@ SELECT
   shop_category.tel,
   shop_category.holiday,
   shop_category.seats,
-  shop_category.price_rage,
+  shop_category.price_range,
   shop_category.score,
   shop_category.geolocation_latitude,
   shop_category.geolocation_longitude,
@@ -18,7 +18,8 @@ FROM
   FROM
   (
     SELECT * FROM t_shop WHERE id = ?
-  ) as t_shopLEFT JOIN t_shop_category ON shop.id = t_shop_category.shop_id
+  ) as shop
+  LEFT JOIN t_shop_category ON shop.id = t_shop_category.shop_id
 ) as shop_category
 LEFT JOIN m_category ON shop_category.category_id = m_category.id
 GROUP BY shop_category.id
