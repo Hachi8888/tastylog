f0bc29557fb411c7d204bea3b899756e7e48277a
Hachi8888 <h.nana.420@gmail.com>
2022-03-17 23:40:48 +0900

    wip

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
+const { authenticate, authorize, PRIVILEGE } = require("../lib/security/accesscontrol.js");
 
 router.get("/login", (req, res, next) => {
-  res.render("./account/login.ejs");
+  // エラーメッセージはconnect flashのデータを取ってきて反映させる
+  res.render("./account/login.ejs", { "message": req.flash("message")});
 });
+
+// 認証処理
+router.use("./login", authenticate());
 
 router.use("/reviews" , require("./account.reviews.js"));
 
