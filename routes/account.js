61dae9dcb557e298e2a031abbead67dfd6b144de
Hachi8888 <h.nana.420@gmail.com>
2022-03-18 23:51:49 +0900

    fixed text place of error message

--- routes/account.js
+++ routes/account.js
@@ -3,11 +3,11 @@ const { authenticate, authorize, PRIVILEGE } = require("../lib/security/accessco
 
 router.get("/login", (req, res, next) => {
   // エラーメッセージはconnect flashのデータを取ってきて反映させる
-  res.render("./account/login.ejs", { "message": req.flash("message")});
+  res.render("./account/login.ejs", { "message": req.flash("message") });
 });
 
 // 認証処理
-router.use("./login", authenticate());
+router.post("./login", authenticate());
 
 router.use("/reviews" , require("./account.reviews.js"));
 
